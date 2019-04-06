bcf40ebd06addfb0c3218d09e335d40e48b19c1d
kmuzalewska <klara.muzalewska@gmail.com>
2019-04-06 16:20:41 +0200

    corected array test & adding files to read

--- magisterka.srcs/sim_1/new/array_of_tcam_tb.vhd
+++ magisterka.srcs/sim_1/new/array_of_tcam_tb.vhd
@@ -13,16 +13,10 @@ end;
 architecture bench of array_of_tcam_tb is
 
     constant T : time := 20 ns;
-    constant num_of_clocks : integer := 4; 
+    constant num_of_clocks : integer := 0; 
     signal i : integer := 0; -- loop variable
     file output_buf : text; -- text is keyword
-    type test_vectors_type is array (3 downto 0) of std_logic_vector(DATA_SIZE-1 downto 0);
-    constant test_vectors : test_vectors_type:= (
-        "00000000101000111010101000010000",
-        "01111000000100011111010110010010",
-        "11011010101110000010000001011011",
-        "01101000111010101011000010100111"
-        );
+    file input_buf : text;
 
   component array_of_tcam
    Port (
@@ -52,42 +46,75 @@ clk_proc: process
         wait for T/2;
   end process clk_proc;
   
-  stimulus: process
+  read_and_pass: process
+    variable read_col_from_input_buf : line;
+    variable test_vector : std_logic_vector(DATA_SIZE-1 downto 0);
   begin
-       wait for T;  
-        -- store 30 samples in file
-        if (i = num_of_clocks) then
-            file_close(output_buf);
---            wait until clk'event and clk='1';
-        else
-            DATA_IN <= test_vectors(i);
-            i <= i + 1;
-        end if;
-        end process;
+      
+      file_open(input_buf, "files/input.txt", read_mode);
+      file_open(output_buf, "files/output.txt", write_mode);
 
-    file_open(output_buf, "counter_data.csv", write_mode);
+      while not endfile(input_buf) loop
+        wait for T;
+        readline(input_buf, read_col_from_input_buf);
+        read(read_col_from_input_buf, test_vector);
+
+        DATA_IN <= test_vector;
+
+      end loop;
+
+      file_close(input_buf);
+      file_close(output_buf);
+      wait;
+
+  end process read_and_pass;
+
+
+  
+  take_and_write: process(clk)
+    variable write_col_to_output_buf : line;
+    variable test_vector : std_logic_vector(DATA_SIZE-1 downto 0);
+  begin
+      
+      if(clk'event and clk='1') then
+
+        write(write_col_to_output_buf, DATA_IN);
+        write(write_col_to_output_buf, string(", "); 
+
+        for j in DATA_OUT'range loop
+          write(write_col_to_output_buf, to_integer(signed(DATA_OUT(j))));
+        end loop;
+        writeline(output_buf, write_col_to_output_buf);
+        --file_close(output_buf);
+
+        wait;
+      end if;
+
+  end process take_and_write;
+
+    --file_open(output_buf, "counter_data.csv", write_mode);
     
-    process(clk)
-        variable write_col_to_output_buf : line; -- line is keyword
-    begin
-        if(clk'event and clk='1') then  -- avoid reset data
-            -- comment below 'if statement' to avoid header in saved file
-            if (i = 0) then 
-              write(write_col_to_output_buf, string'("data_in,data_out"));
-              writeline(output_buf, write_col_to_output_buf);
-            end if; 
+--    process(clk)
+--        variable write_col_to_output_buf : line; -- line is keyword
+--    begin
+--        if(clk'event and clk='1') then  -- avoid reset data
+--            -- comment below 'if statement' to avoid header in saved file
+--            if (i = 0) then 
+--              write(write_col_to_output_buf, string'("data_in,data_out"));
+--              writeline(output_buf, write_col_to_output_buf);
+--            end if; 
 
-            write(write_col_to_output_buf, DATA_IN);
-            write(write_col_to_output_buf, string'(","));
-            -- Note that unsigned/signed values can not be saved in file, 
-            -- therefore change into integer or std_logic_vector etc.
-             -- following line saves the count in integer format
-             for j in DATA_OUT'range loop
-                write(write_col_to_output_buf,to_integer(signed(DATA_OUT(j))) ); 
---                writeline(output_buf, write_col_to_output_buf);
-              end loop;
-        end if;
-    end process;
+--            write(write_col_to_output_buf, DATA_IN);
+--            write(write_col_to_output_buf, string'(","));
+--            -- Note that unsigned/signed values can not be saved in file, 
+--            -- therefore change into integer or std_logic_vector etc.
+--             -- following line saves the count in integer format
+--             for j in DATA_OUT'range loop
+--                write(write_col_to_output_buf,to_integer(signed(DATA_OUT(j))) ); 
+----                writeline(output_buf, write_col_to_output_buf);
+--              end loop;
+--        end if;
+--    end process;
 
 
 --    wait;
